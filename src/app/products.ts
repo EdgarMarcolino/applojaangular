faae2d35b304e61e9ec79b297c26c95180e1f524
EdgarMarcolino <128751690+EdgarMarcolino@users.noreply.github.com>
2023-05-12 19:20:44 -0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,41 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'IPHONE',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'MOTOROLA',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'SAMSUNG',
+    price: 299,
+    description: ''
+  },
+
+  {
+    id: 4,
+    name: 'XIAOMI',
+    price: 699,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
